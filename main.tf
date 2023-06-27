1362495060986ba94419103d1bc6b45cd4f53eae
Wictor Zhao <wictor_x@hotmail.com>
2023-06-27 11:00:12 +0200

    Add s3 prefix
    Ref: TINFRA-313

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ resource "aws_cur_report_definition" "this" {
   format                     = "Parquet"
   compression                = "Parquet"
   additional_schema_elements = ["RESOURCES"]
+  s3_prefix                  = var.central_s3_bucket
   s3_bucket                  = var.central_s3_bucket
   s3_region                  = "eu-west-1"
   additional_artifacts       = ["ATHENA"]
