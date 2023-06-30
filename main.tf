70d38ebba92c5a3d0d871b0b1bb4d60771a4a821
Wictor Zhao <wictor_x@hotmail.com>
2023-06-30 15:01:05 +0200

    Add new rules for master account replication

--- main.tf
+++ main.tf
@@ -10,39 +10,19 @@ terraform {
 }
 
 provider "aws" {
-  alias  = "us-east-1"
-  region = "us-east-1"
-}
-
-
-/*
- * == Cost reporting
- */
-
-resource "aws_cur_report_definition" "this" {
-  provider                   = aws.us-east-1
-  report_name                = "${var.source_account_id}-cur-definition"
-  time_unit                  = "HOURLY"
-  format                     = "Parquet"
-  compression                = "Parquet"
-  additional_schema_elements = ["RESOURCES"]
-  s3_prefix                  = aws_s3_bucket.cost_reporter.id
-  s3_bucket                  = aws_s3_bucket.cost_reporter.id
-  s3_region                  = "eu-west-1"
-  additional_artifacts       = ["ATHENA"]
-  report_versioning          = "OVERWRITE_REPORT"
+  region = "eu-west-1"
 }
 
 /*
  * == S3 cost reporter bucket to be replicated
 */
 
-resource "aws_s3_bucket" "cost_reporter" {
-  bucket = "${var.source_account_id}-source-cur"
+data "aws_s3_bucket" "cost_reporter" {
+  bucket = "crayon-vy-prod-bucket"
 }
 
 resource "aws_s3_bucket_versioning" "cost_reporter_versioning" {
-  bucket = aws_s3_bucket.cost_reporter.id
+  bucket = data.aws_s3_bucket.cost_reporter.id
   versioning_configuration {
     status = "Enabled"
   }
@@ -59,7 +39,7 @@ data "aws_iam_policy_document" "assume_role" {
 
     principals {
       type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
+      identifiers = ["s3.amazonaws.com", "batchoperations.s3.amazonaws.com"]  # for batch operations
     }
 
     actions = ["sts:AssumeRole"]
@@ -67,8 +47,9 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "replication" {
-  name               = "cur-replication-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  name                 = "cur-replication-role"
+  assume_role_policy   = data.aws_iam_policy_document.assume_role.json
+  permissions_boundary = "arn:aws:iam::276520083766:policy/Boundary"
 }
 
 data "aws_iam_policy_document" "replication" {
@@ -77,22 +58,27 @@ data "aws_iam_policy_document" "replication" {
 
     actions = [
       "s3:GetReplicationConfiguration",
+      "s3:PutInventoryConfiguration", # For batch operation replication
       "s3:ListBucket",
     ]
 
-    resources = [aws_s3_bucket.cost_reporter.arn]
+    resources = [data.aws_s3_bucket.cost_reporter.arn]
   }
 
   statement {
     effect = "Allow"
 
     actions = [
+      "s3:InitiateReplication", # batch operations
+      "s3:GetObject", # batch operations
+      "s3:GetObjectVersion", # batch operations
+      "s3:PutObject", # batch operations
       "s3:GetObjectVersionForReplication",
       "s3:GetObjectVersionAcl",
       "s3:GetObjectVersionTagging",
     ]
 
-    resources = ["${aws_s3_bucket.cost_reporter.arn}/*"]
+    resources = ["${data.aws_s3_bucket.cost_reporter.arn}/*"]
   }
 
   statement {
@@ -105,7 +91,7 @@ data "aws_iam_policy_document" "replication" {
       "s3:ObjectOwnerOverrideToBucketOwner"
     ]
 
-    resources = ["arn:aws:s3:::${var.central_account_id}-central-cost-and-usage-reports/*"]
+    resources = ["arn:aws:s3:::846274634169-central-cost-and-usage-reports/*"]
   }
 }
 
@@ -124,15 +110,15 @@ resource "aws_s3_bucket_replication_configuration" "replication" {
   depends_on = [aws_s3_bucket_versioning.cost_reporter_versioning]
 
   role   = aws_iam_role.replication.arn
-  bucket = aws_s3_bucket.cost_reporter.id
+  bucket = data.aws_s3_bucket.cost_reporter.id
 
   rule {
     status = "Enabled"
     id     = "cur_replication_rule"
 
     destination {
-      account       = var.central_account_id
-      bucket        = "arn:aws:s3:::${var.central_account_id}-central-cost-and-usage-reports"
+      account       = "846274634169"
+      bucket        = "arn:aws:s3:::846274634169-central-cost-and-usage-reports"
       storage_class = "STANDARD"
       access_control_translation {
         owner = "Destination"
@@ -147,47 +133,8 @@ resource "aws_s3_bucket_replication_configuration" "replication" {
 */
 
 resource "aws_s3_bucket_ownership_controls" "source" {
-  bucket = aws_s3_bucket.cost_reporter.id
+  bucket = data.aws_s3_bucket.cost_reporter.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
-}
-
-data "aws_iam_policy_document" "reporter_policy" {
-  statement {
-    effect  = "Allow"
-    actions = [
-      "s3:GetBucketAcl",
-      "s3:GetBucketPolicy"
-    ]
-
-    principals {
-      type        = "Service"
-      identifiers = [
-        "billingreports.amazonaws.com"
-      ]
-    }
-
-    resources = [aws_s3_bucket.cost_reporter.arn]
-  }
-  statement {
-    effect  = "Allow"
-    actions = [
-      "s3:PutObject",
-    ]
-
-    principals {
-      type        = "Service"
-      identifiers = [
-        "billingreports.amazonaws.com"
-      ]
-    }
-
-    resources = ["${aws_s3_bucket.cost_reporter.arn}/*"]
-  }
-}
-
-resource "aws_s3_bucket_policy" "reporter_policy" {
-  bucket = aws_s3_bucket.cost_reporter.id
-  policy = data.aws_iam_policy_document.reporter_policy.json
 }
--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "central_account_id" {
-  type = string
-}
-
-variable "source_account_id" {
-  type = string
-}
